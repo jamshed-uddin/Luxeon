508bb46a1a16deb0d00628789e80cc94c54e3e02
Jami <jamsheduddin03@gmail.com>
2025-02-04 00:37:58 +0600

    product details page data fetching fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-// import AboutUs from "@/components/homepage/AboutUs";
-// import FAQs from "@/components/homepage/FAQs";
+import AboutUs from "@/components/homepage/AboutUs";
+import FAQs from "@/components/homepage/FAQs";
 import Hero from "@/components/homepage/Hero";
-// import OurCategories from "@/components/homepage/OurCategories";
-// import OurProducts from "@/components/homepage/OurProducts";
+import OurCategories from "@/components/homepage/OurCategories";
+import OurProducts from "@/components/homepage/OurProducts";
 
 export default async function Home() {
   return (
     <div className="-mt-16 space-y-14 lg:space-y-20 hide-scrollbar ">
       <Hero />
-      {/* <OurProducts />
+      <OurProducts />
       <AboutUs />
       <OurCategories />
-      <FAQs /> */}
+      <FAQs />
     </div>
   );
 }
--- src/lib/getProductData.ts
+++ src/lib/getProductData.ts
@@ -1,16 +1,12 @@
 import { cache } from "react";
 import { Product } from "./definition";
+import { requestClient } from "./requestClient";
 
 export const getProductData = cache(async (id: string): Promise<Product> => {
   try {
-    const res = await fetch(`http://localhost:4000/api/products/${id}`);
+    const product = await requestClient<Product>(`/products/${id}`);
 
-    if (!res.ok) {
-      throw new Error("Something went wrong!");
-    }
-
-    const product = await res.json();
-    return product as Product;
+    return product;
   } catch (error) {
     throw error;
   }
