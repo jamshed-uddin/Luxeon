87e505b03c2a32cd9366c03f8f4106a63e2af02c
Jami <jamsheduddin03@gmail.com>
2025-02-03 23:23:49 +0600

    replaced axios with fetch

--- src/lib/requestClient.ts
+++ src/lib/requestClient.ts
@@ -1,20 +1,26 @@
-import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import { AxiosError } from "axios";
 
 export const requestClient = async <T>(
   url: string,
-  options: AxiosRequestConfig
+  options: RequestInit
 ): Promise<T> => {
   const serverBaseUrl = process.env.NEXT_PUBLIC_SERVER_BASE_URL as string;
 
   const apiUrl = `${serverBaseUrl}${url}`;
 
   try {
-    const response = await axios({
-      url: apiUrl,
-      withCredentials: true,
+    // const response = await axios({
+    //   url: apiUrl,
+    //   withCredentials: true,
+    //   ...options,
+    // });
+
+    const response = await fetch(apiUrl, {
       ...options,
     });
-    return response.data as T;
+
+    const data = await response.json();
+    return data as T;
   } catch (error) {
     if (error instanceof AxiosError) {
       throw error?.response?.data;
